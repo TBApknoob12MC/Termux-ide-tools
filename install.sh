ea88f88b73cf2114d649d3c6e6a8c10987a8d699
TBApknoob12MC <144533355+TBApknoob12MC@users.noreply.github.com>
2024-12-01 12:42:13 +0530

    Add cmatrix
    Title

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 clear && echo "LET'S INSTALL"
 termux-setup-storage
 apt update && apt upgrade -y
-apt install zsh make nmap tmux proot-distro asciinema gotty gnupg unzip neovim git tig python3 wget figlet nodejs newsboat irssi lynx -y 
+apt install zsh make nmap tmux proot-distro asciinema cmatrix gotty gnupg unzip neovim git tig python3 wget figlet nodejs newsboat irssi lynx -y 
 echo 'clear && GREEN="\e[92m"&& printf "${GREEN}" && figlet -f standard "TERMUX" && STOP="\e[0m" && printf "${STOP}" && androfetch' >> $PREFIX/bin/cls
 chmod +x $PREFIX/bin/cls
 mkdir $HOME/.newsboat
