76e9505aeddd66de5581f0291f9932bd0b83c8a0
TBApknoob12MC <144533355+TBApknoob12MC@users.noreply.github.com>
2024-12-01 12:26:01 +0530

    Update ncfe-install.sh

--- ncfe-install.sh
+++ ncfe-install.sh
@@ -2,5 +2,5 @@ apt update && apt install nmap figlet wget -y
 rm -f $PREFIX/bin/ncat-fe
 mkdir $HOME/.ncat-fe
 mkdir $HOME/.ncat-fe/html
-wget -O $PREFIX/bin/ncat-fe https://raw.githubusercontent.com/TBApknoob12MC/Termux-improver/main/ncat-fe
+wget -O $PREFIX/bin/ncat-fe https://raw.githubusercontent.com/TBApknoob12MC/Termux-improver/main/ncat-fe.sh
 chmod +x $PREFIX/bin/ncat-fe
