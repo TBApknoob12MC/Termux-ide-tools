f8dad33f1a5b3acad2cfaf189b24773f8d6398b4
TBApknoob12MC <144533355+TBApknoob12MC@users.noreply.github.com>
2025-02-09 17:29:28 +0530

    Fix link err

--- ncfe-install.sh
+++ ncfe-install.sh
@@ -2,5 +2,5 @@ apt update && apt install nmap figlet wget -y
 rm -f $PREFIX/bin/ncat-fe
 mkdir $HOME/.ncat-fe
 mkdir $HOME/.ncat-fe/html
-wget -O $PREFIX/bin/ncat-fe https://raw.githubusercontent.com/TBApknoob12MC/Termux-improver/main/ncat-fe.sh
+wget -O $PREFIX/bin/ncat-fe https://raw.githubusercontent.com/TBApknoob12MC/Termux-ide-tools/main/ncat-fe.sh
 chmod +x $PREFIX/bin/ncat-fe
